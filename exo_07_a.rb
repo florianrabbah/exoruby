91663e9b8548de634fbc6ee672c46e781afefa02
florianrabbah <floflo97231@live.fr>
2023-10-03 00:44:15 +0200

    Ajout des exo ruby

---
+++ exo_07_a.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp #supprime le caractère /n
+puts user_name
---
+++ exo_12.rb
@@ -0,0 +1,9 @@
+puts "Entrez un nombre :"
+nombre = gets.chomp.to_i
+
+puts "Comptage :"
+
+# Utilisation de la boucle for pour compter de 1 jusqu'au nombre spécifié
+for i in 1..nombre
+  puts i
+end
